1066c3192a8444e1745bac12f0864d011c358a92
Yuanyuan Zhong <a6510c@motorola.com>
2013-01-02 12:33:31 -0600

    staging: android: persistent_ram: free temporary buffer
    When there is no valid old persistent_ram buffer, we need to free
    the temporary buffers which are supposed to be merged into old
    persistent_ram buffer.
    
    Change-Id: I2fc06dbb78a98f0734fc866fa7ef92a140578bb7
    Signed-off-by: Yuanyuan Zhong <a6510c@motorola.com>
    Reviewed-on: http://gerrit.pcs.mot.com/508951
    Tested-by: Jira Key <JIRAKEY@motorola.com>
    Reviewed-by: Jeffrey Carlyle <jeff.carlyle@motorola.com>
    Reviewed-by: Igor Kovalenko <cik009@motorola.com>
    (cherry picked from commit f540c1b3bf666ee1a5f288e76e2774293b3ed5e9)

--- drivers/staging/android/ram_console.c
+++ drivers/staging/android/ram_console.c
@@ -382,13 +382,14 @@ static int __init ram_console_late_init(void)
 	bool is_last_bldr_ramdump_mode = false;
 #endif
 
+	persistent_ram_ext_oldbuf_merge(prz);
+
 	if (!prz)
 		return 0;
 
 	if (persistent_ram_old_size(prz) == 0)
 		return 0;
 
-	persistent_ram_ext_oldbuf_merge(prz);
 	entry = create_proc_entry("last_kmsg", S_IFREG | S_IRUGO, NULL);
 	if (!entry) {
 		printk(KERN_ERR "ram_console: failed to create proc entry\n");
