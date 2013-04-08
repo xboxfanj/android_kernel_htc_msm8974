0ba4545a24ac518196ba3c44f67f85f3ffd1f5cb
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-08 16:45:40 +0530

    [LINARO] workqueues: Introduce new flag WQ_POWER_EFFICIENT for power oriented workqueues
    Adapted for 3.4 from lsk-v3.10
    
    Workqueues can be performance or power-oriented. Currently, most workqueues are
    bound to the CPU they were created on. This gives good performance (due to cache
    effects) at the cost of potentially waking up otherwise idle cores (Idle from
    scheduler's perspective. Which may or may not be physically idle) just to
    process some work. To save power, we can allow the work to be rescheduled on a
    core that is already awake.
    
    Workqueues created with the WQ_UNBOUND flag will allow some power savings.
    However, we don't change the default behaviour of the system.  To enable
    power-saving behaviour, a new config option CONFIG_WQ_POWER_EFFICIENT needs to
    be turned on. This option can also be overridden by the
    workqueue.power_efficient boot parameter.
    
    tj: Updated config description and comments.  Renamed
        CONFIG_WQ_POWER_EFFICIENT to CONFIG_WQ_POWER_EFFICIENT_DEFAULT.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    (cherry picked from commit cee22a15052faa817e3ec8985a28154d3fabc7aa)
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>
    
    Conflicts:
    	kernel/workqueue.c
    
    Conflicts:
    	kernel/workqueue.c
    
    Conflicts:
    	include/linux/workqueue.h
    	kernel/workqueue.c
    
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -42,6 +42,7 @@
 #include <linux/lockdep.h>
 #include <linux/idr.h>
 #include <linux/bug.h>
+#include <linux/moduleparam.h>
 
 #include "workqueue_sched.h"
 
@@ -210,6 +211,15 @@ struct workqueue_struct {
 #endif
 	char			name[];		
 };
+
+/* see the comment above the definition of WQ_POWER_EFFICIENT */
+#ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
+static bool wq_power_efficient = true;
+#else
+static bool wq_power_efficient;
+#endif
+
+module_param_named(power_efficient, wq_power_efficient, bool, 0444);
 
 struct workqueue_struct *system_wq __read_mostly;
 struct workqueue_struct *system_long_wq __read_mostly;
@@ -1973,6 +1983,10 @@ struct workqueue_struct *__alloc_workqueue_key(const char *fmt,
 	struct workqueue_struct *wq;
 	unsigned int cpu;
 	size_t namelen;
+
+	/* see the comment above the definition of WQ_POWER_EFFICIENT */
+	if ((flags & WQ_POWER_EFFICIENT) && wq_power_efficient)
+		flags |= WQ_UNBOUND;
 
 	
 	va_start(args, lock_name);
