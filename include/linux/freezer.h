10f4972f4ae7891b539cfeab90c3c695ecd5957c
Iliyan Malchev <malchev@google.com>
2013-07-01 12:43:47 -0700

    Revert "freezer: comment out might_sleep()"
    This reverts commit 6cdb09deefb68b9eaeb63ec052103c366fa9e5dc.
    
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>
    
    Conflicts:
    	include/linux/freezer.h
    
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -35,6 +35,7 @@ extern void thaw_kernel_threads(void);
 
 static inline bool try_to_freeze(void)
 {
+	might_sleep();
 	if (likely(!freezing(current)))
 		return false;
 	return __refrigerator(false);
