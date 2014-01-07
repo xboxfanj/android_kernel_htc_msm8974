bc0aa01fd8f10dd1581127965f4958a10724f547
Terje Bergstrom <tbergstrom@nvidia.com>
2014-01-07 13:28:14 +0200

    devfreq: Fix type of frequency table
    Frequencies are always expressed as ulong, but devfreq's table is of
    type uint *. Fix the mismatch.
    
    Change-Id: I0ed6b43f397331d1940c7f573d37796e50e8ac58
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/352720
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    Signed-off-by: Jake Weinstein <xboxlover360@gmail.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -49,7 +49,7 @@ struct devfreq_dev_profile {
 	int (*get_cur_freq)(struct device *dev, unsigned long *freq);
 	void (*exit)(struct device *dev);
 
-	unsigned int *freq_table;
+	unsigned long *freq_table;
 	unsigned int max_state;
 	const struct devfreq_governor_data *governor_data;
 	unsigned int num_governor_data;
