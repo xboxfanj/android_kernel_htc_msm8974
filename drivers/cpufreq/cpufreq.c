63e7100bc9dd6a617f360b947b77017e608aa1cd
Jake Weinstein <xboxlover360@gmail.com>
2014-08-17 17:28:03 -0500

    cpufreq: fix dumb typo

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -559,7 +559,7 @@ cpufreq_freq_attr_ro(scaling_cur_freq);
 cpufreq_freq_attr_ro(bios_limit);
 cpufreq_freq_attr_ro(related_cpus);
 cpufreq_freq_attr_ro(affected_cpus);
-pufreq_freq_attr_ro(cpu_utilization);
+cpufreq_freq_attr_ro(cpu_utilization);
 cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
